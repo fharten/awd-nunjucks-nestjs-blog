12397f5998902429ab40f4b8b1678f83f2436643
Dirk <so0mewhat2@gmail.com>
2025-07-23 13:02:45 +0200

    refactor: serve static files in module

--- src/blogEntries/blog.entries.controller.ts
+++ src/blogEntries/blog.entries.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { BlogEntriesService } from './blog.entries.service';
 import { BlogEntries, BlogEntry } from '../types/BlogEntry';
+import { Response } from 'express';
 
 @Controller()
 export class BlogEntriesController {
   constructor(private readonly blogEntriesService: BlogEntriesService) {}
 
   @Get('/')
-  async getAll(): Promise<BlogEntries> {
-    console.log('BlogEntriesController');
+  async getAll(@Res() res: Response): Promise<BlogEntries> {
+    const blogEntries = await this.blogEntriesService.getAllBlogEntries();
+
+    res.render('../views/pages/index.html', {
+      blogEntries,
+      headerData: {
+        image: 'home-bg.jpg',
+        title: 'Clean Blog',
+        subTitle: 'A Blog Theme by Start Bootstrap',
+      },
+    });
     return this.blogEntriesService.getAllBlogEntries();
   }
 
--- src/blogEntries/blog.entries.module.ts
+++ src/blogEntries/blog.entries.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { BlogEntriesController } from './blog.entries.controller';
 import { BlogEntriesService } from './blog.entries.service';
 import { BlogEntriesRepository } from './blog.entries.repository';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'),
+    }),
+  ],
   controllers: [BlogEntriesController],
   providers: [BlogEntriesService, BlogEntriesRepository],
 })
